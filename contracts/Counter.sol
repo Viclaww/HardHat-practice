c81b2881b4e14752e0bac35c631c5dc19e558eae
Victor Lawrence <viclawrence99@gmail.com>
2024-04-26 11:35:27 +0100

    This is it

---
+++ contracts/Counter.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.24;
+
+contract Counter{
+    uint public count;
+
+    function get() public view returns  (uint) {
+        return count;
+    }
+
+
+// function adds to the count 
+    function inc() public {
+        count += 1;
+
+    }
+
+    // this function reduces by 1 
+    // and doesnt works when count is zero
+    function dec() public {
+        count -= 1;
+    }
+}
---
+++ ignition/modules/Counter.js
@@ -0,0 +1,13 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+// const JAN_1ST_2030 = 1893456000;
+// const ONE_GWEI = 1_000_000_000n;
+
+module.exports = buildModule("CounterModule", (m) => {
+  // const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
+  // const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
+
+  const counter = m.contract("Counter");
+  console.log("This is the address", counter.address);
+  return { counter };
+});
